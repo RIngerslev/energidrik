c90d855bb9d69508b0a07b0a95c576175e88dae0
Rasmus Ingerslev <rasprivat@hotmail.com>
2024-10-18 21:34:15 +0200

    nyt

--- app/header.tsx
+++ app/header.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center p-4 h-24 px-10 bg-green-900 shadow">
+    <header className="flex justify-between items-center p-4 h-24 px-10 bg-[#fb0201] shadow">
       <div className="logo">
           <button className='clickEffect'>
-            <Link href="/" className='text-2xl font-semibold text-green-900 bg-white px-2 py-1 rounded'>
+            <Link href="/" className='text-2xl font-semibold text-[#fb0201] bg-white px-2 py-1 rounded'>
                 Rating App
             </Link>
           </button>
