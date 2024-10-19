7f3ac02f8772f087c87885cc94ced24f794570ad
Rasmus Ingerslev <rasprivat@hotmail.com>
2024-10-19 11:27:25 +0200

    new look

--- app/header.tsx
+++ app/header.tsx
@@ -2,18 +2,18 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center p-4 h-24 px-10 bg-[#fb0201] shadow">
+    <header className="fixed top-0 left-0 right-0 flex justify-between items-center p-4 h-24 px-10 bg-[#fb0201] shadow">
       <div className="logo">
-          <button className='clickEffect'>
-            <Link href="/" className='text-2xl font-semibold text-[#fb0201] bg-white px-2 py-1 rounded'>
-                Rating App
-            </Link>
-          </button>
+        <button className='clickEffect'>
+          <Link href="/" className='text-2xl font-semibold text-[#fb0201] bg-white px-2 py-1 rounded'>
+            FuelScore
+          </Link>
+        </button>
       </div>
       <div className="plus">
         <button className="text-2xl font-semibold text-white clickEffect">
           <Link href="/add">
-          +
+            +
           </Link>
         </button>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { deleteDataFirebase, fetchDataFirebase } from "./firebase";
+import lightningIcon from "../public/lightningIcon.svg";
 import Header from "./header";
 import BottomNavBar from "./bottomNavBar";
+import Rating from "./rating";
 
 export default function Home() {
 
@@ -23,22 +25,24 @@ export default function Home() {
     <div className="">
       <Header />
       <div className="col-span-1 mb-5">
-        <div className="flex flex-col items-center justify-center"></div>
+        <div className="flex flex-col items-center justify-center mt-16"></div>
+        <li>
         {drinks.map((drink) => (
-          <div key={drink.id} className="flex flex-col-2 space-x-4 justify-between bg-white h-30 rounded-xl shadow-xl mt-5 p-3 mx-4 lg:mx-72 overflow-hidden">
+          <ul key={drink.id} className="flex flex-col-2 space-x-4 justify-between bg-white h-30 rounded-xl shadow-xl mt-5 p-3 mx-4 lg:mx-72 overflow-hidden">
             <div className="flex-1">
               <h1>{drink.name}</h1>
               <div className="flex">
-              <svg height="20px" width="20px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" 
-                  viewBox="0 0 512 512" xmlSpace="preserve">
-                <polygon fill="#FFD248" points="392.111,222.395 195.711,470.784 253.476,280.16 114.84,280.16 224.593,8.665 340.123,8.665 
-                  253.476,222.395 "/>
-                <path d="M174.167,512l67.63-223.176H101.992L218.751,0h134.235l-86.647,213.73h143.669L174.167,512z M127.689,271.495h137.467
-                  l-47.9,158.072l156.959-198.508H240.614l86.647-213.73h-96.824L127.689,271.495z"/>
-                </svg>
+                <Image src={lightningIcon} alt="Star Icon" width={20} height={20} />
+                <h2>(2)</h2>
               </div>
-              <h2>Taste: {drink.taste}</h2>
-              <h2>Look: {drink.look}</h2>
+              <div className="flex">              
+                <h2>Taste: </h2>
+                <Rating className="flex" count={6} value={drink.taste} />
+              </div>
+              <div className="flex">              
+                <h2>Look: </h2>
+                <Rating className="flex" count={6} value={drink.look} />
+              </div>
             </div>
             <div className="flex-2 justify-end items-center h-full">
               <Image
@@ -52,8 +56,9 @@ export default function Home() {
                 }}
               />
             </div>
-          </div>
+          </ul>
         ))}
+        </li>
       </div>
       <BottomNavBar />
     </div>
