86ee06cc7fe19690136fa16556933d539eb97744
Rasmus Ingerslev <rasprivat@hotmail.com>
2024-11-13 21:17:03 +0100

    igen

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,5 +1,5 @@
 // pages/product/[id].tsx
-
+"server";
 import Image from "next/image";
 import Link from "next/link";
 import { fetchSingleDocumentFirebase } from "../../firebase/firebase"; // Adjust the path if necessary
